178567930bd06ef7cf6ae846ad6a114b928abec5
Aleh Savonevich <savonevich.dem@gmail.com>
2016-09-01 23:10:59 +0300

    Worked with client's authentication

---
+++ auth/client/src/actions/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import {
+    AUTH_USER,
+    AUTH_ERROR,
+    UNAUTH_USER
+} from './types';
+
+const ROOT_URL = 'http://localhost:3090';
+
+export function signinUser({ email, password }) {
+    return function(dispatch) {
+        axios.post(`${ROOT_URL}/signin`, { email, password })
+            .then((response) => {
+                dispatch({ type: AUTH_USER });
+
+                localStorage.setItem('token', response.data.token);
+
+                browserHistory.push('/feature');
+            })
+            .catch(() => {
+                dispatch(authError('Bad login info'));
+            });
+    };
+}
+
+export function signOutUser() {
+    localStorage.removeItem('token');
+
+    return {
+        type: UNAUTH_USER
+    }
+}
+
+export function authError(error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    }
+}
